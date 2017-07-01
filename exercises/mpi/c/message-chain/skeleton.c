a5741805ed291c14638294da0bae23e2ef0df5c8
mjalava <mika.jalava@gmail.com>
2017-07-01 15:47:01 +0300

    exercise Sat 3a

--- exercises/mpi/c/message-chain/skeleton.c
+++ exercises/mpi/c/message-chain/skeleton.c
@@ -31,12 +31,14 @@ int main(int argc, char *argv[])
     /* TODO start */
     /* Send and receive messages as defined in exercise */
     if (myid < ntasks - 1) {
+      MPI_Send(message,size,MPI_INT,myid+1,0,MPI_COMM_WORLD);
 
         printf("Sender: %d. Sent elements: %d. Tag: %d. Receiver: %d\n",
                myid, size, myid + 1, myid + 1);
     }
 
     if (myid > 0) {
+      MPI_Recv(receiveBuffer,size,MPI_INT,myid-1,0,MPI_COMM_WORLD,&status);
 
         printf("Receiver: %d. first element %d.\n",
                myid, receiveBuffer[0]);

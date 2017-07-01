f7218eb2cc2e5d26871d8310afb3cc2bfc5549a2
mjalava <mika.jalava@gmail.com>
2017-07-01 15:35:05 +0300

    Saturday exercise 1

--- exercises/mpi/c/hello-world/hello.c
+++ exercises/mpi/c/hello-world/hello.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-#pragma omp parallel
-    {
-        printf("X\n");
-    }
 
+  int rank,ncomms;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&ncomms);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  printf("Hello world from %d!\n",rank);
+  if(rank == 0)
+    printf("Total %d mpi processes\n",ncomms);
+
+  MPI_Finalize();
     return 0;
 }

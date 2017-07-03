8c32eacdd163a6cd9101841950ab90db3ee4e25f
mjalava <mika.jalava@gmail.com>
2017-07-03 14:20:58 +0300

    exercise 12 a

--- exercises/mpi/c/posix-io/spokesman.c
+++ exercises/mpi/c/posix-io/spokesman.c
@@ -50,10 +50,43 @@ void single_writer(int my_id, int *localvector, int localsize)
 {
     FILE *fp;
     int *fullvector;
+    int i, ntasks;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+
+    if(my_id == WRITER_ID)
+      {
+	fullvector = (int *) malloc(DATASIZE * sizeof(int));
+	for(i = WRITER_ID*localsize; i < (WRITER_ID+1)*localsize; i++)
+	  fullvector[i] = localvector[i];
+      }
+
+    for(i = 0; i < ntasks; i++)
+      {
+	printf("%d\n",i);
+	if(i != WRITER_ID && my_id == WRITER_ID)
+	  MPI_Recv(fullvector + i * localsize,localsize,MPI_INT,i,99,
+		   MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	else if(i != WRITER_ID && my_id == i)
+	  MPI_Send(localvector,localsize,MPI_INT,WRITER_ID,99,MPI_COMM_WORLD);
+
+
+      }
+
+
+    if(my_id == WRITER_ID)
+      {
+	fp = fopen("outfile","w");
+	for(i = 0; i < DATASIZE; i++)
+	  {
+	    fprintf(fp,"%d %d\n",i,fullvector[i]);
+	  }
+      }
 
     /* TODO: Implement a function that will write the data to file so that
        a single process does the file io. Use rank WRITER_ID as the io rank */
-
-    free(fullvector);
+    
+    if(my_id == WRITER_ID)
+      free(fullvector);
 }
 
